bed5a95f24006a90652d5ad60ce36ad88d6706b7
wanderson aragão <wanderson.eq2011@gmail.com>
2023-11-18 10:32:17 -0300

    FEATURE 4

--- src/main/java/br/edu/infnet/selllingApp/controller/SellingDetailsController.java
+++ src/main/java/br/edu/infnet/selllingApp/controller/SellingDetailsController.java
@@ -1,5 +1,6 @@
 package br.edu.infnet.selllingApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -9,11 +10,12 @@ import br.edu.infnet.selllingApp.model.service.SellingService;
 @Controller
 public class SellingDetailsController {
 
+	@Autowired
 	private SellingService sellingService;
 
 	@PostMapping(value = "/selling_details/add")
 	public String insert(SellingDetails sellingDetails) {
-//		System.out.println(sellingDetails.getField() + "-" + sellingDetails.getDescription());
+		System.out.println("INPUTS: " + sellingDetails.getField() + "-" + sellingDetails.getDescription());
 		SellingDetails res = sellingService.sendDetails(sellingDetails);
 		System.out.println("RESPONSE FROM SELLING DETAILS: " + res);
 		return "redirect:/";
